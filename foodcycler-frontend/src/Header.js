f3631774e85ae395dd7a32927c404a5cb2181115
Denis Elezi <deniselezi2003@outlook.com>
2023-02-25 16:05:37 +0000

    adding navbar

---
+++ foodcycler-frontend/src/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+import './Header.css';
+
+
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">FoodCycler</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto my-navbar">
+            <div>
+                <Nav.Link href="#fridge">Fridge</Nav.Link>
+                <Nav.Link href="#recipes">Recipes</Nav.Link>
+                <Nav.Link href="#market">Market</Nav.Link>
+                <Nav.Link href="#savings">Savings</Nav.Link>
+            </div>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
