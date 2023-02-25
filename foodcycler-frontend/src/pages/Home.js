f89fc5a73db5e0e53f89c53f4e077cbe1a3db074
Denis Elezi <deniselezi2003@outlook.com>
2023-02-25 16:59:14 +0000

    added react-router

---
+++ foodcycler-frontend/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default Home;
