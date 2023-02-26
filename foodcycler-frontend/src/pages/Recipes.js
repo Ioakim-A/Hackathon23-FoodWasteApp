afb1971bf50c92a48e1a5f19ff7ed333eebcb64a
Szym <91729762+Szymoony@users.noreply.github.com>
2023-02-26 09:55:26 +0000

    Added carbon footpirnt and css

--- foodcycler-frontend/src/pages/Recipes.js
+++ foodcycler-frontend/src/pages/Recipes.js
@@ -1,128 +1,92 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./css/Recipes.css";
 
 function Recipes() {
   const [searchQuery, setSearchQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
 
-  const YOUR_APP_ID = "0ab2f586";
-  const YOUR_APP_KEY = "773e8bb9f8e3f7bd000ce0ec241c32e8";
+  const appId = "0ab2f586";
+  const appKey = "773e8bb9f8e3f7bd000ce0ec241c32e8";
 
-  const handleSearch = () => {
-    fetch(
-      `https://api.edamam.com/search?q=${searchQuery}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const recipesWithNutrition = data.hits.slice(0, 5).map((hit) => {
-          const recipe = hit.recipe;
-          const totalNutrients = recipe.totalNutrients;
-
-          const nutrition = {
-            calories: Math.round(totalNutrients.ENERC_KCAL.quantity),
-            fat: Math.round(totalNutrients.FAT.quantity),
-            carbs: Math.round(totalNutrients.CHOCDF.quantity),
-            protein: Math.round(totalNutrients.PROCNT.quantity),
-          };
-
-          return {
-            ...recipe,
-            nutrition,
-          };
-        });
-
-        setRecipes(recipesWithNutrition);
-
-        // fetch carbon footprint for each recipe
-        const recipeUrls = recipesWithNutrition.map((recipe) => recipe.url);
-
-        Promise.all(
-          recipeUrls.map((url) =>
-            fetch(
-              `https://api.eaternity.org/food/carbon?ingredient=${encodeURIComponent(
-                url
-              )}`
-            )
-          )
-        )
-          .then((responses) =>
-            Promise.all(responses.map((response) => response.json()))
-          )
-          .then((carbonData) => {
-            const recipesWithCarbon = recipesWithNutrition.map(
-              (recipe, index) => ({
-                ...recipe,
-                carbonFootprint: Math.round(
-                  carbonData[index]?.carbon_footprint_per_kg || 0
-                ),
-              })
-            );
-
-            setRecipes(recipesWithCarbon);
-          })
-          .catch((error) => console.error(error));
-      })
-      .catch((error) => console.error(error));
-  };
-
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      handleSearch();
+  const searchRecipes = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${appId}&app_key=${appKey}&beta=true`
+      );
+      const newRecipes = response.data.hits.slice(0, 5).map((hit) => {
+        const { label, image, ingredientLines, url } = hit.recipe;
+        const { ENERC_KCAL, PROCNT, FAT, CHOCDF } = hit.recipe.totalNutrients;
+        const co2EmissionsClass = hit.recipe.co2EmissionsClass;
+        return {
+          label,
+          image,
+          ingredientLines,
+          nutrients: {
+            calories: Math.round(ENERC_KCAL.quantity),
+            protein: Math.round(PROCNT.quantity),
+            fat: Math.round(FAT.quantity),
+            carbs: Math.round(CHOCDF.quantity),
+          },
+          carbonRating: co2EmissionsClass,
+          url,
+        };
+      });
+      setRecipes(newRecipes);
+    //   setSearchQuery("");
+    } catch (error) {
+      console.error(error);
     }
   };
 
   return (
-    <div className="Recipes">
-      <h1 className="Recipes-header">Search Recipes</h1>
-      <div className="Recipes-search">
+    <div className="recipes">
+      <form onSubmit={searchRecipes}>
         <input
           type="text"
-          placeholder="Enter a search query"
+          placeholder="Search Recipes by Ingredients"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          onKeyPress={handleKeyPress}
-          className="Recipes-search-input"
         />
-        <button onClick={handleSearch} className="Recipes-search-button">
-          Search
-        </button>
-      </div>
-      <div className="Recipes-list">
-        {recipes.map((recipe, index) => (
-          <div key={index} className="Recipes-item">
-            <img
-              src={recipe.image}
-              alt={recipe.label}
-              className="Recipes-item-image"
-            />
-            <div className="Recipes-item-details">
-              <h3 className="Recipes-item-title">{recipe.label}</h3>
-              <ul className="Recipes-item-nutrition">
-                <li>Calories: {recipe.nutrition.calories} Kcal</li>
-                <li>Fat: {recipe.nutrition.fat}g</li>
-                <li>Carbs: {recipe.nutrition.carbs}g</li>
-                <li>Protein: {recipe.nutrition.protein}g</li>
-                
-              </ul>
-              <ul className="Recipes-item-ingredients">
-                {recipe.ingredientLines.map((ingredient, index) => (
-                  <li key={index} className="Recipes-item-ingredient">
-                    {ingredient}
-                  </li>
-                ))}
-              </ul>
-              <a
-                href={recipe.url}
-                target="_blank"
-                rel="noreferrer"
-                className="Recipes-item-link"
-              >
-                View Recipe
-              </a>
-            </div>
+        <button type="submit">Search</button>
+      </form>
+      {recipes.map((recipe, index) => (
+        <div key={index} className="recipe">
+          <h3>{recipe.label}</h3>
+          <a href={recipe.url} target="_blank" rel="noopener noreferrer">
+            <img src={recipe.image} alt={recipe.label} />
+          </a>
+          <ul>
+            {recipe.ingredientLines.map((ingredient, i) => (
+              <li key={i}>{ingredient}</li>
+            ))}
+          </ul>
+          <div className="nutrition">
+            <h4>Nutritional Values:</h4>
+            <ul>
+              <li>Calories: {recipe.nutrients.calories} kcal</li>
+              <li>Protein: {recipe.nutrients.protein} g</li>
+              <li>Fat: {recipe.nutrients.fat} g</li>
+              <li>Carbohydrates: {recipe.nutrients.carbs} g</li>
+            </ul>
           </div>
-        ))}
-      </div>
+          <div className={`carbon-rating carbon-rating-${recipe.carbonRating}`}>
+            <h4>Carbon Rating:</h4>
+            <p
+              className={`carbon-rating-text ${
+                recipe.carbonRating === "A+" || recipe.carbonRating === "A"
+                  ? "carbon-rating-low"
+                  : recipe.carbonRating === "B" || recipe.carbonRating === "C"
+                  ? "carbon-rating-medium"
+                  : "carbon-rating-high"
+              }`}
+            >
+              {recipe.carbonRating}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
