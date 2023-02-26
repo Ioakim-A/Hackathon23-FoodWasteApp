735d257c1198904f3533fd763bf49acf5fc1f79c
Denis Elezi <deniselezi2003@outlook.com>
2023-02-26 01:40:58 +0000

    added popups for markers

--- foodcycler-frontend/src/components/Marker.js
+++ foodcycler-frontend/src/components/Marker.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 
+import './css/Marker.css';
+
+
 const Marker = (props) => {
   const { lat, lng } = props;
-  console.log(lat, lng)
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleMarkerClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
-    <div style={{ position: 'absolute', transform: 'translate(-50%, -50%)' }}>
-      <FaMapMarkerAlt color="red" size={30} />
+    <div style={{ position: 'relative', transform: 'translate(-50%, -50%)' }}>
+      <FaMapMarkerAlt color="red" size={30} onClick={handleMarkerClick}/>
+      {showPopup && (
+        <div className="seller">
+          <p><b>Food for sale</b></p>
+          <p>Seller information</p>
+          <a href="#" style={{ display: 'block', paddingBottom: '10px' }}>
+            Contact seller for purchase
+          </a>
+          <button onClick={handleClosePopup}>Close</button>
+        </div>
+      )}
     </div>
   );
 };
