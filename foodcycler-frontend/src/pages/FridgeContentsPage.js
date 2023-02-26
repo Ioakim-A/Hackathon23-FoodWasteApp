e79688c6bd44a6ac697e3e13b99b6200ae4ab9a1
Ioakim <ioakim.avraam.21@ucl.ac.uk>
2023-02-26 10:56:12 +0000

    Fridge UI improvements

--- foodcycler-frontend/src/pages/FridgeContentsPage.js
+++ foodcycler-frontend/src/pages/FridgeContentsPage.js
@@ -7,7 +7,7 @@ import chicken from "../assets/Chicken Breast.png";
 import apple from "../assets/apple.png";
 import orange from "../assets/orange.png";
 import milk from "../assets/milk.png";
-import steak from "../assets/Steak.png";
+import steak from "../assets/steak.png";
 import fridgeBackground from "../assets/fridge_background.png";
 
 
@@ -34,7 +34,7 @@ const FridgeContentsPage = () => {
     "apple": apple,
     "orange": orange,
     "milk": milk,
-    "Steak": steak,
+    "steak": steak,
   };
 
   useEffect(() => {
@@ -56,57 +56,60 @@ const FridgeContentsPage = () => {
   const displayFridgeItems = () => {
     const items = [];
     let row = [];
+    let itemCount =0;
 
     fridgeContents.forEach((item) => {
       const img = (
         <img
           src={fridgeItems[item.item_name]}
           alt={item.item_name}
-          style={{ width: "250px", height: "250px" }}
+          style={{ width: "200px", height: "200px" }}
         />
       );
       const name = <h3>{capitalizeFirstLetter(item.item_name)}</h3>;
       const qty = (
         <div>
           <label>Quantity:</label>
-          <input type="number" value={item.item_qty} />
+          <input type="number" value={item.item_qty} className="inputLine"/>
         </div>
       );
       const weight = (
-        <div>
-          <label>Weight:</label>
-          <input type="number" value={item.item_weight} />
+        <div style={{marginRight:"6px"}}>
+          <label>Weight(g):</label>
+          <input type="number" value={item.item_weight} className="inputLine"/>
         </div>
       );
 
       row.push(
-        <div key={item.item_name} style={{ display: "inline-block", marginLeft: "25px", backgroundColor: "rgb(0,0,0,0.1)", paddingBottom:"20px",paddingLeft:"20px",paddingRight:"20px" }}>
-          {img}
-          {name}
-          {qty}
-          {weight}
-        </div>
+          <div key={item.item_name} style={{ display: "inline-block", marginLeft: "25px", marginTop: "25px", backgroundColor: "rgb(0,0,0,0.1)", paddingBottom:"20px",paddingLeft:"20px",paddingRight:"20px", textAlign: "center" }}>
+            {img}
+            {name}
+            {qty}
+            {weight}
+          </div>
       );
 
-      if (row.length === 3) {
-        items.push(<div key={items.length}>{row}</div>);
-        row = [];
-      }
-    });
+      itemCount++;
 
-    if (row.length > 0) {
+    if (itemCount % 5 === 0) {
       items.push(<div key={items.length}>{row}</div>);
+      row = [];
     }
+  });
 
-    return items;
-  };
+  if (row.length > 0) {
+    items.push(<div key={items.length}>{row}</div>);
+  }
 
-  return (
-    <div style={styles.fridgeBackground}>
-      <h1 className="title">Fridge Contents</h1>
-      {displayFridgeItems()}
-    </div>
-  );
+  return items;
+};
+
+return (
+  <div style={styles.fridgeBackground}>
+    <h1 className="title">Fridge Contents</h1>
+    <div>{displayFridgeItems()}</div>
+  </div>
+);
 };
 
 export default FridgeContentsPage;
